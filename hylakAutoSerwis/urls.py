c840cee7fd287e2b6f2f11a1284445aaa5e09f9f
maciej chylinski <maciejx.chylinski@intel.com>
2018-02-05 13:09:11 +0100

    Project setup. Added Post model

---
+++ hylakAutoSerwis/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'hylakAutoSerwis.views.home', name='home'),
+<<<<<<< HEAD
+    # url(r'^blog/', include('blog.urls')),
+
+=======
+    #url(r'^blog/', include('blog.urls')),
+
+    #url(r'^store/', include('store.urls'), name='store'),
+>>>>>>> views_customization
+    url(r'^admin/', include(admin.site.urls)),
+]
